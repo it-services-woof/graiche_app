26303087837a4b8d5c7014a5e2da89450fee333d
wesleycurldev <wesley@curldev.com.br>
2024-10-30 14:43:57 -0300

    fix px

--- src/components/SectionVideoHero.tsx
+++ src/components/SectionVideoHero.tsx
@@ -3,7 +3,6 @@ import { Header } from "./Header";
 export function SectionVideoHero() {
 	return (
 		<section className="relative flex w-full h-[932px] md:h-[1117px] overflow-hidden">
-			{/* Vídeo Desktop */}
 			<div className="hidden md:block absolute inset-0 overflow-hidden">
 				<iframe
 					src="https://player.vimeo.com/video/1013634411?muted=1&autoplay=1&loop=1&background=1&app_id=122963"
@@ -12,7 +11,6 @@ export function SectionVideoHero() {
 					className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto transform -translate-x-1/2 -translate-y-1/2 scale-125"
 				/>
 			</div>
-			{/* Vídeo Mobile */}
 			<div className="md:hidden absolute inset-0 overflow-hidden">
 				<iframe
 					src="https://player.vimeo.com/video/1022153063?muted=1&autoplay=1&loop=1&background=1&app_id=122963"
@@ -21,10 +19,8 @@ export function SectionVideoHero() {
 					className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto transform -translate-x-1/2 -translate-y-1/2 scale-110"
 				/>
 			</div>
-			{/* Conteúdo Sobreposto */}
-			<div className="relative z-10 w-full pt-[60px] px-[40px] md:pt-[90px] md:px-[160px]">
+			<div className="relative z-10 w-full pt-[60px] px-[20px] md:pt-[90px] md:px-[160px]">
 				<Header />
-				{/* Outros conteúdos */}
 			</div>
 		</section>
 	);
